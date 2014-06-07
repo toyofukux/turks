0b87dfaa4c200ecb66f88d1f7a75f94ff082845c
takasing <takaty2011@gmail.com>
2014-06-07 21:25:00 +0900

    add mixi utils.

---
+++ mixi/main.rb
@@ -0,0 +1,12 @@
+require 'logger'
+require './auth'
+require './user'
+
+logger = Logger.new 'logs/authenticate.log'
+logger.level = Logger::INFO
+
+user = Turks::MixiUser.new
+auth = Turks::MixiAuth.new
+auth.authenticate(user.email, user.password)
+logger.info auth.mecha.page
+
---
+++ mixi/user.rb
@@ -0,0 +1,15 @@
+require 'yaml'
+
+module Turks
+  class MixiUser
+    USER_YAML = 'config/user.yaml'
+    attr_reader :email
+    attr_reader :password
+    def initialize
+      user = YAML.load_file USER_YAML
+      @email = user["mixi"]["email"]
+      @password = user["mixi"]["password"]
+    end
+  end
+end
+
