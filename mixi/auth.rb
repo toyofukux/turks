4f9028cdcf71b7d902184474091ac2782bac68b2
SET_ME_LOCALLY <SET_ME_LOCALLY>
2014-06-07 21:25:00 +0900

    add mixi utils.

---
+++ mixi/auth.rb
@@ -0,0 +1,23 @@
+require 'mechanize'
+require 'nokogiri'
+require 'logger'
+
+module Turks
+  class MixiAuth
+    MIXI_LOGIN_URL = 'https://mixi.jp/'
+    attr_reader :mecha
+    def initialize
+      @mecha = Mechanize.new { |agent| 
+        agent.user_agent_alias = 'Mac Safari'
+        agent.follow_meta_refresh = true
+      }
+    end
+    def authenticate(email, password, next_url='/home.pl')
+      @mecha.get(MIXI_LOGIN_URL)
+      @mecha.page.form_with(name: 'login_form') { |form|
+        form.set_fields(next_url: next_url, email: email, password: password)
+      }.click_button
+    end
+  end
+end
+
