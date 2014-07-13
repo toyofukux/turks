5484aed8ebab1a86bbd6d0b21709978fff36aa11
takasing <takaty2011@gmail.com>
2014-07-13 22:41:08 +0900

    mod text, and enable to handle long reply.

--- tw_bot/lib/bot.rb
+++ tw_bot/lib/bot.rb
@@ -10,9 +10,14 @@ rest_client = client.rest_client
 str_client = client.streaming_client
 str_client.user(replies: "all") do |obj|
   if obj.is_a?(Twitter::Tweet) && BOT_SCREEN_NAME == obj.in_reply_to_screen_name
-    replied = obj.text.delete("@#{BOT_SCREEN_NAME}").strip
-    text = "@#{obj.user.screen_name} 会社は学校じゃねぇんだよ！「#{replied}」とか言ってんじゃねぇ！"
-    rest_client.update(text, in_reply_to_status_id: obj.id)
-    logger.info "replied. text=#{text}"
+    text = "@#{obj.user.screen_name} 会社は学校じゃねぇんだよ！"
+    begin
+      tweet = rest_client.update("#{text}「#{obj.text.delete("@#{BOT_SCREEN_NAME}").strip}」とか言ってんじゃねぇ！元ネタ→http://ameblo.jp/junpei-1114/entry-11889404525.html", in_reply_to_status_id: obj.id)
+      logger.info "replied. text=#{tweet.text}, status_id=#{tweet.id}"
+    rescue => e
+      logger.error e.backtrace
+      tweet = rest_client.update(text, in_reply_to_status_id: obj.id)
+      logger.info "replied. text=#{tweet.text}, status_id=#{tweet.id}"
+    end
   end
 end
