f3b94b4d5c9de1978e78e36e517a4f34bfcf4666
takasing <takaty2011@gmail.com>
2014-07-13 17:52:40 +0900

    custom replied text.

--- tw_bot/lib/bot.rb
+++ tw_bot/lib/bot.rb
@@ -3,13 +3,15 @@ require 'logger'
 require './lib/client'
 
 logger = Logger.new 'logs/bot.log'
+BOT_SCREEN_NAME = "is_not_bot"
 
 client = Turks::Client.new
 rest_client = client.rest_client
 str_client = client.streaming_client
 str_client.user(replies: "all") do |obj|
-  if obj.is_a?(Twitter::Tweet) && "is_not_bot" == obj.in_reply_to_screen_name
-    text = "@#{obj.user.screen_name} 会社は学校じゃねぇんだよ！"
+  if obj.is_a?(Twitter::Tweet) && BOT_SCREEN_NAME == obj.in_reply_to_screen_name
+    replied = obj.text.delete("@#{BOT_SCREEN_NAME}").strip
+    text = "@#{obj.user.screen_name} 会社は学校じゃねぇんだよ！「#{replied}」とか言ってんじゃねぇ！"
     rest_client.update(text, in_reply_to_status_id: obj.id)
     logger.info "replied. text=#{text}"
   end
