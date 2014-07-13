03f72924b9a5e3f2501c533eef56ed9b5608f4ee
takasing <takaty2011@gmail.com>
2014-07-13 17:15:15 +0900

    mod text.

--- tw_bot/lib/bot.rb
+++ tw_bot/lib/bot.rb
@@ -6,7 +6,7 @@ p client
 rest_client = client.rest_client
 str_client = client.streaming_client
 str_client.user(replies: "all") do |obj|
-  if obj.is_a?(Twitter::Tweet) && "toyodev" == obj.in_reply_to_screen_name
-    rest_client.update("@#{obj.user.screen_name} 会社は学校じゃねえ！", in_reply_to_status_id: obj.id)
+  if obj.is_a?(Twitter::Tweet) && "is_not_bot" == obj.in_reply_to_screen_name
+    rest_client.update("@#{obj.user.screen_name} 会社は学校じゃねぇんだよ！", in_reply_to_status_id: obj.id)
   end
 end
