10c52feb8548c794d59de3a16c7b1accfda3aecf
takasing <takaty2011@gmail.com>
2014-07-13 17:26:46 +0900

    out log.

--- tw_bot/lib/bot.rb
+++ tw_bot/lib/bot.rb
@@ -1,12 +1,16 @@
 require 'twitter'
+require 'logger'
 require './lib/client'
 
+logger = Logger.new 'logs/bot.log'
+
 client = Turks::Client.new
-p client
 rest_client = client.rest_client
 str_client = client.streaming_client
 str_client.user(replies: "all") do |obj|
   if obj.is_a?(Twitter::Tweet) && "is_not_bot" == obj.in_reply_to_screen_name
-    rest_client.update("@#{obj.user.screen_name} 会社は学校じゃねぇんだよ！", in_reply_to_status_id: obj.id)
+    text = "@#{obj.user.screen_name} 会社は学校じゃねぇんだよ！"
+    rest_client.update(text, in_reply_to_status_id: obj.id)
+    logger.info "replied. text=#{text}"
   end
 end
