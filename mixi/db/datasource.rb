0c9bd76a26fbdb695fc17b1aceb9f971e9360dc9
takasing <takaty2011@gmail.com>
2014-08-15 02:16:43 +0900

    refactor activerecord settings.

--- mixi/db/datasource.rb
+++ mixi/db/datasource.rb
@@ -6,8 +6,9 @@ module Turks
     DATASOURCE_CONFIG = File.expand_path('../../config/database.yml', __FILE__)
     def self.connenction
       config = YAML.load_file(DATASOURCE_CONFIG)
-      environment = ENV["ENVIRONMENT"] ? ENV["ENVIRONMENT"] : "development"
+      environment = ENV["ENV"] ? ENV["ENV"] : "development"
       ActiveRecord::Base.establish_connection(config["db"][environment])
+      ActiveRecord::Base.logger = Logger.new(STDOUT)
     end
   end
 end
